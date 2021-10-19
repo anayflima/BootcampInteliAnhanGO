3afe0d09ccff6ba3bad96c563921568775918994
morais1244 <86611025+morais1244@users.noreply.github.com>
2021-10-18 23:59:20 -0300

    main com AI do oponente

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -5,9 +5,13 @@ var ball = BALL_NODE.instance()
 
 const DEFAULT_Y = 150
 
+var rng = RandomNumberGenerator.new()
+
 const p1_x = 45
 var p1_y = DEFAULT_Y
 var p1_score = 0
+#var can_walk = true
+#var timer = null
 
 const p2_x = 990
 var p2_y = DEFAULT_Y
@@ -21,6 +25,7 @@ const SPACE_TO_PLAY = "Aperte espaco para jogar"
 const P1_WIN = "Voce venceu"
 const P2_WIN = "Voce perdeu"
 var message = SPACE_TO_PLAY
+var my_random_number = rng.randi_range(150, 300)
 
 func _ready():
 	set_ball()
@@ -28,7 +33,15 @@ func _ready():
 	$t2.set_paddle_position(p2_x, p2_y)
 	display_message()
 	update_score()
-
+	
+#	timer = Timer.new()
+#	timer.set_one_shot(true)
+#	timer.set_wait_time(my_random_number)
+#	timer.connect("timeout",self,"timeout_is_completed")
+#	add_child(timer)
+#
+#func timeout_is_completed():
+#	can_walk = true
 
 func _input(_event):
 	if Input.is_key_pressed(KEY_SPACE):
@@ -73,11 +86,21 @@ func handle_movement_input(delta):
 		p1_y -= 300 * delta
 	if Input.is_key_pressed(KEY_S):
 		p1_y += 300 * delta
-	if Input.is_key_pressed(KEY_UP):
-		p2_y -= 300 * delta
-	if Input.is_key_pressed(KEY_DOWN):
-		p2_y += 300 * delta
-
+	# IA 
+	if abs(ball.position.y - p2_y) > 25:
+		if ball.position.y > p2_y :
+			p2_y += my_random_number * delta
+		else:
+			p2_y -= my_random_number * delta
+#	if abs(ball.position.y - p2_y) > 25 && can_walk:
+#		while ball.position.y > p2_y :
+#			p2_y += 300 * delta
+#			can_walk = false
+#			timer.start()
+#		while ball.position.y < p2_y : 
+#			p2_y -= 300 * delta
+#			can_walk = false
+#			timer.start()
 
 func handle_score_event():
 	if score_event:
