22baeb20f546432b36e09b720b26dd159aef8074
morais1244 <86611025+morais1244@users.noreply.github.com>
2021-10-19 00:00:40 -0300

    scenes atualizadas e corrigidas

--- Scenes/t2.gd
+++ Scenes/t2.gd
@@ -1,12 +1,13 @@
 extends KinematicBody2D
 
 
-func _ready():
-	pass
+
 
 func set_paddle_position(x, y):
-	if y <= 55:
-		y = 56
-	if y >= 800 - 250:
-		y = 799 - 250
-	self.position = Vector2(x, y)
+	if y <= 50:
+		y = 51
+	if y >= 793 - 250:
+		y = 792 - 250
+	self.position = Vector2(x,y)
+
+
