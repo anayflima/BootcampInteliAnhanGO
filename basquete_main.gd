b9c7534b5c5a7df3c73a9d594718b4b1ffce0373
Guilherme <g251109@dac.unicamp.br>
2021-10-25 18:43:40 -0300

    adição de vetor com angulo e mais labels

--- basquete_main.gd
+++ basquete_main.gd
@@ -7,43 +7,59 @@ var count = 0
 var verif = true
 var verif_1 = true
 var point = true
+var radius = 50
+var slides = true
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$button_play_again.visible = false
 	$botaoJogar.visible = true
 	$button_get_out.visible = false
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	x = $KinematicBody2D/bodyball.position.x
 	y = $KinematicBody2D/bodyball.position.y
+	var angle = deg2rad($Node2D/angleslide.value)
+	var from = Vector2(15+x,y)
+	var to = get_point(angle,radius)
+	print($Node2D/angleslide.value)
+	print($Node2D/forceslide.value)
+	
+	#valores de acerto = 60, 275  // 66, 297 // 62, 281
+	
 	if y>348:
 		count += 1
 	if count == 150:
 		$button_play_again.visible = true
 		$button_get_out.visible = true
 
-	if (y<0):
+	if (y<-10):
 		$button_play_again.visible = true
 		$button_get_out.visible = true
-	if verif:
-		$Node2D/forceslide.value += 1
-		if $Node2D/forceslide.value == $Node2D/forceslide.max_value :
-			verif = false
-	if !verif :
-		$Node2D/forceslide.value -= 1
-		if $Node2D/forceslide.value == $Node2D/forceslide.min_value:
-			verif = true
-	if verif_1:
-		$Node2D/angleslide.value += 1
-		if $Node2D/angleslide.value == $Node2D/angleslide.max_value :
-			verif_1 = false
-	if !verif_1 :
-		$Node2D/angleslide.value -= 1
-		if $Node2D/angleslide.value == $Node2D/angleslide.min_value:
-			verif_1 = true
-	$DisplayMessage2.text = str(Globals.score)
+	if slides :
+		if verif:
+			$Node2D/forceslide.value += 1
+			if $Node2D/forceslide.value == $Node2D/forceslide.max_value :
+				verif = false
+		if !verif :
+			$Node2D/forceslide.value -= 1
+			if $Node2D/forceslide.value == $Node2D/forceslide.min_value:
+				verif = true
+		if verif_1:
+			$Node2D/angleslide.value += 1
+			
+			draw_line_angle(from,to)
+			
+			if $Node2D/angleslide.value == $Node2D/angleslide.max_value :
+				verif_1 = false
+		if !verif_1 :
+			$Node2D/angleslide.value -= 1
+			draw_line_angle(from,to)
+			if $Node2D/angleslide.value == $Node2D/angleslide.min_value:
+				verif_1 = true
+	$pontuacao.text = str(Globals.score)
 	point()
 
 
@@ -56,6 +72,10 @@ func _on_Button_pressed():
 	$KinematicBody2D/bodyball.apply_impulse(offset,impulse)
 	$KinematicBody2D/AnimationPlayer.play("jogando")
 	$botaoJogar.visible = false
+	slides = false
+	$linha.clear_points()
+	
+	
 	
 func get_impulse(angle,size):
 	angle = angle*PI/180
@@ -78,3 +98,18 @@ func _on_button_play_again_pressed():
 
 func _on_button_get_out_pressed():
 	get_tree().change_scene("res://map.tscn")
+
+
+func get_point(angle,radius):
+	var c = cos(angle)
+	var s = sin(angle)
+	x = $KinematicBody2D/bodyball.position.x
+	y = $KinematicBody2D/bodyball.position.y
+	var point = Vector2(15+x+c*radius,y-s*radius)
+	return point
+
+func draw_line_angle(from, to):
+
+	$linha.clear_points()
+	$linha.add_point(from)
+	$linha.add_point(to)
