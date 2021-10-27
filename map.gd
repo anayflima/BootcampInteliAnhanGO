089dc1ba403d37c94ee5d77cd59beb38f0aabb01
morais1244 <86611025+morais1244@users.noreply.github.com>
2021-10-27 15:26:00 -0300

    botão por aproximação pingpong

--- map.gd
+++ map.gd
@@ -1,3 +1,4 @@
+
 extends Node2D
 
 
@@ -6,12 +7,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Button.visible = false
+	$button_pingpong.visible = false
 	
 func _process(delta):
 	var x = $KinematicBody2D.position.x
 	var y = $KinematicBody2D.position.y
 	if x > 172 && x < 1002 && y > 1012 && y < 1534:
-		$Button.visible = true
+		$button_pingpong.visible = true
 	if !(x > 172 && x < 1002 && y > 1012 && y < 1534):
-		$Button.visible = false
+		$button_pingpong.visible = false
