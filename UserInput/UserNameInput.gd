d7aa9892c66b031b858048302ce854bcae712088
Ana Yoon <ayf.lima@gmail.com>
2021-10-29 12:09:10 -0300

    Muda interface da história inicial, acrescentando cenas e mudando caixa de diálogo

--- UserInput/UserNameInput.gd
+++ UserInput/UserNameInput.gd
@@ -12,5 +12,5 @@ func _on_Button2_pressed():
 	get_tree().change_scene("res://start menu.tscn")
 	
 func _on_Button_pressed():
-	get_tree().change_scene("res://historia_inicial/initial_story_scene_1.tscn")
+	get_tree().change_scene("res://historia_inicial/rpgtext.tscn")
 
