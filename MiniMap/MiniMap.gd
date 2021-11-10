332ff95e23c735ad6ded374bea6e20e505907b56
Ana Yoon <ayf.lima@gmail.com>
2021-11-10 14:39:35 -0300

    Coloca minimapa no canto superior do mapa principal

--- MiniMap/MiniMap.gd
+++ MiniMap/MiniMap.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func _ready():
-	$ViewportContainer/Viewport/Node2D/KinematicBody2D/Camera2D.zoom=Vector2(8,8)
+	$CanvasLayer/ViewportContainer/Viewport/Node2D/KinematicBody2D/Camera2D.zoom=Vector2(8,8)
 	pass
