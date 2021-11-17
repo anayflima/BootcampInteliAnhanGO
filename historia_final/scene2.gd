489778bfe5f200d4085378035fd2cdcb2c398156
Ana Yoon <ayf.lima@gmail.com>
2021-11-17 18:02:46 -0300

    Muda icone do jogo, arruma animacao do redemoinho e conserta change scene da historia final

--- historia_final/scene2.gd
+++ historia_final/scene2.gd
@@ -75,8 +75,5 @@ func _on_Tween_tween_completed(object, key):
 	change_state(State.FINISHED)
 
 func _on_Button2_pressed():
-	get_tree().change_scene("res://historia_final/rpgtext.tscn")
+	get_tree().change_scene("res://historia_final/scene1.tscn")
 
-func _on_Button_pressed():
-	pass
-	#get_tree().change_scene("res://historia_final/scene3.tscn")
