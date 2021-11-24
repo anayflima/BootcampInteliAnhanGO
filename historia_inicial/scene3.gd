236ee209bb5f7cd8e8744c9e90d2d28dcf03dee7
Ana Yoon <ayf.lima@gmail.com>
2021-11-24 12:33:44 -0300

    Muda velocidade de aparecimento dos dialogos e textos na historia final e inicial

--- historia_inicial/scene3.gd
+++ historia_inicial/scene3.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-const CHAR_READ_RATE = 0.05
+const CHAR_READ_RATE = 0.03
 onready var textbox_container = $TextboxContainer
 onready var start_symbol = $TextboxContainer/MarginContainer/HBoxContainer/Start
 onready var end_symbol = $TextboxContainer/MarginContainer/HBoxContainer/End
